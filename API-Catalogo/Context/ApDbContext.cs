2d70a2713a21f5f67de2f1d1b6ede206d7ef2ea0
Victor Osório <victorporto_@hotmail.com>
2023-02-28 10:05:54 -0300

    Adicionar arquivos de projeto.

---
+++ API-Catalogo/Context/ApDbContext.cs
@@ -0,0 +1,13 @@
+﻿using API_Catalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Catalogo.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+        public DbSet<Categoria>? Categorias { get; set; }   
+        public DbSet<Produto>? Produtos { get; set; }   
+    }
+}
